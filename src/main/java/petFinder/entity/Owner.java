65cc345d302f5be2ff2220b2bbe8fa2872fa0340
alex <colo.alexandru@yahoo.com>
2022-03-30 22:29:58 +0300

    "changed strategy for Id"

--- src/main/java/petFinder/entity/Owner.java
+++ src/main/java/petFinder/entity/Owner.java
@@ -15,7 +15,7 @@ import java.util.Set;
 public class Owner {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
