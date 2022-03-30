cc987c3c596c1a5dba9cf365a6a3a997bcf3f7cc
alex <colo.alexandru@yahoo.com>
2022-03-30 19:11:12 +0300

    added find pet by breed

--- src/main/java/petFinder/repository/PetRepository.java
+++ src/main/java/petFinder/repository/PetRepository.java
@@ -2,8 +2,14 @@ package petFinder.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
 import petFinder.entity.Pet;
+
+import java.util.Set;
 
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
+
+    Set<Pet> findByBreed(String breed);
+
 }
