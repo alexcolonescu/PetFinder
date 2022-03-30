d7ddb74dc2df1e212e4acbbb41c505d79e7d6b60
alex <colo.alexandru@yahoo.com>
2022-03-30 19:04:47 +0300

    added City entity for pet identification

---
+++ src/main/java/petFinder/entity/City.java
@@ -0,0 +1,5 @@
+package petFinder.entity;
+
+public enum City {
+    Alba, Arad, Arges, Bacau, Bihor, BistritaNasaud, Botosani, Brasov, Braila, Bucuresti, Buzau, CarasSeverin, Calarasi, Cluj, Constanta, Covasna, Dambovita, Dolj, Galati, Giurgiu, Gorj, Harghita, Hunedoara, Ialomita, Iasi, Ilfov, Maramures, Mehedinti, Mures, Neamt, Olt, Prahova, SatuMare, Salaj, Sibiu, Suceava, Teleorman, Timis, Tulcea, Vaslui, Valcea, Vrancea;
+}
