323afa9acaacf876097e3c6b611c1a363b187bd0
alex <colo.alexandru@yahoo.com>
2022-03-30 22:27:48 +0300

    "deleted city from entity"

--- src/main/java/petFinder/entity/ContactDetails.java
+++ src/main/java/petFinder/entity/ContactDetails.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class ContactDetails {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 10, unique = true)
