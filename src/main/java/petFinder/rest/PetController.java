90cfc386f8f6c85a841c41ff99e11b3ed39bd513
alex <colo.alexandru@yahoo.com>
2022-04-02 13:44:48 +0300

    handle situations when pets belong to the same owners

--- src/main/java/petFinder/rest/PetController.java
+++ src/main/java/petFinder/rest/PetController.java
@@ -2,7 +2,11 @@ package petFinder.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import petFinder.entity.ContactDetails;
+import petFinder.entity.Owner;
 import petFinder.entity.Pet;
+import petFinder.exceptions.ContactsAlreadyExist;
+import petFinder.repository.ContactDetailsRepository;
 import petFinder.repository.OwnerRepository;
 import petFinder.repository.PetRepository;
 
@@ -18,8 +22,24 @@ public class PetController {
     @Autowired
     private OwnerRepository ownerRepository;
 
+    @Autowired
+    private ContactDetailsRepository contactDetailsRepository;
+
     @PostMapping(value = "/pet")
     public Pet savePet(@RequestBody Pet pet){
+        for (Owner owner: pet.getOwners()) {
+            ContactDetails foundContact = contactDetailsRepository.findByPhoneNumber(owner.getContactDetails().getPhoneNumber());
+            if(foundContact != null) {
+                //throw new ContactsAlreadyExist("Contacts already exist");
+                owner.setContactDetails(foundContact);
+                owner.setId(foundContact.getOwner().getId());
+            }
+            else {
+                owner.setContactDetails(contactDetailsRepository.save(owner.getContactDetails()));
+                ownerRepository.save(owner);
+            }
+        }
+
         return petRepository.save(pet);
     }
 
@@ -33,9 +53,9 @@ public class PetController {
         petRepository.deleteById(id);
     }
 
-    @GetMapping(value = "/pet/breed/{breed}")
-    public Set<Pet> getPetByName(@PathVariable(name = "breed") String breed){
-        return petRepository.findByBreedLike(breed);
+    @GetMapping(value = "/pet/breed")
+    public Set<Pet> getPetByName(@RequestParam(name = "breed") String breed){
+        return petRepository.findByBreedContaining(breed);
     }
 
 
