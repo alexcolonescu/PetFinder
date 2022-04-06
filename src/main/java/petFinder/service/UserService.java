42b0f7bfbccbbde6cec30add364c69cd1b34a736
alex <colo.alexandru@yahoo.com>
2022-04-06 13:49:16 +0300

    Added User Service for saving passwords, finding users by email or username & password

---
+++ src/main/java/petFinder/service/UserService.java
@@ -0,0 +1,23 @@
+package petFinder.service;
+
+import org.springframework.stereotype.Service;
+import petFinder.entity.MyUser;
+
+import java.util.List;
+
+@Service
+public interface UserService {
+
+    MyUser findUserByEmail(String email);
+
+    MyUser findUserByUserName(String userName);
+
+    boolean findUserByUserNameAndPassword(String userName, String password);
+
+    List<MyUser> findAll();
+
+    void deleteById(long id);
+
+    MyUser saveUser(MyUser u);
+
+}
