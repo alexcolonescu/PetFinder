7d2c2a9b49a999436128af9992e6cdcbc79d1ca6
alex <colo.alexandru@yahoo.com>
2022-05-08 17:57:01 +0300

    - deleted Owner Controller, Owner Repository

--- src/main/java/petFinder/repository/OwnerRepository.java
+++
@@ -1,9 +0,0 @@
-package petFinder.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import petFinder.entity.Owner;
-
-public interface OwnerRepository extends JpaRepository<Owner, Long> {
-    Owner findByLastNameAndFirstName(String lastName, String firsName);
-
-}
