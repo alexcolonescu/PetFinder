7f4082c4e0ca6a46c6ef65641e27b2ccdd6b14ad
alex <colo.alexandru@yahoo.com>
2022-05-08 18:03:24 +0300

    - Changed ID from Long to Integer - firstName & lastName => name - deleted Contact Details and added a new attribute (phoneNumber) - added some Constructors for Owner entity - added Getter & Setters for the attributes

--- src/main/java/petFinder/entity/Owner.java
+++ src/main/java/petFinder/entity/Owner.java
@@ -1,38 +1,70 @@
 package petFinder.entity;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import lombok.*;
-
 import javax.persistence.*;
-import java.util.Set;
 
 @Entity
-@Getter
-@Setter
-@ToString
 public class Owner {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
-    @Column(nullable = false)
-    private String firstName;
-
-    @Column(nullable = false)
-    private String lastName;
+    @Column(length = 45, nullable = false, unique = true)
+    private String name;
 
     @Column(nullable = false, length = 30, unique = true)
     private String email;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    private ContactDetails contactDetails;
+    @Column(nullable = false, length = 30, unique = true)
+    private String phoneNumber;
 
-    @ToString.Exclude
-    @ManyToMany(mappedBy = "owners")
-    @JsonIgnore
-    private Set<Pet> pets;
+    public Owner(){
+    }
 
+    public Owner(Integer id){
+        this.id = id;
+    }
+
+    public Owner(String name){
+        this.name = name;
+    }
+
+    public Owner(String name, String email, String phoneNumber){
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail(){ return email;}
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPhoneNumber(){ return phoneNumber;}
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
 }
