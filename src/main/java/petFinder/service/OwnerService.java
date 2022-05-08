ac09583cb161455a31f31fe6506cd683de57d7c9
alex <colo.alexandru@yahoo.com>
2022-05-08 18:11:49 +0300

    Added "OwnerService" that is dealing with ".csv" files and add the csv file into Owner database

---
+++ src/main/java/petFinder/service/OwnerService.java
@@ -0,0 +1,38 @@
+package petFinder.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import petFinder.entity.Owner;
+import petFinder.repository.OwnerRepository;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+@Service
+public class OwnerService {
+
+    @Autowired
+    private OwnerRepository ownerRepository;
+
+    String line = "";
+
+    public void saveOwnerData(){
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/static/Owners.csv"));
+            while((line=br.readLine())!=null){
+                String [] data = line.split(";");
+                Owner owner = new Owner();
+                owner.setName(data[0]);
+                owner.setEmail(data[1]);
+                owner.setPhoneNumber(data[2]);
+                ownerRepository.save(owner);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
