b114754ca2f8ff219719f8ddca46bf31eccbcd28
alex <colo.alexandru@yahoo.com>
2022-05-08 18:12:04 +0300

    Created a basic test

---
+++ src/test/java/petFinder/PetFinderAppTest.java
@@ -0,0 +1,13 @@
+package petFinder;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PetFinderAppTest {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
